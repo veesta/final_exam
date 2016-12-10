d2b2dc9aac884da359f5470b41b1d3c5be069009
veesta <vmavanda@uoguelph.ca>
2016-12-10 17:12:14 -0500

    Update exam_script_part1.R

--- exam_script_part1.R
+++ exam_script_part1.R
@@ -7,15 +7,15 @@ power = .85
 
 #my.f2 = sr2 / (1 - r2)
 
-my.f2 <- .01 / (1 - .20)
+my.f2 <- .1 / (1 - .20)
 
 print (my.f2)
 
-#f2 = .0125
+#f2 = .125
 
 #u = 1 since we are only looking at one predictor right now 
-pwr.f2.test(u=1, f2=.0125, power=.85)
+pwr.f2.test(u=1, f2=.125, power=.85)
 
-N= 1 + 719 +1 
+N= 2 + 719 +1 
 
 print(N)
